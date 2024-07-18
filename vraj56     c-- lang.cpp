a99874fc8f7326eeaa0ff380014df7dad05f1af0
vraj9023215049 <vraj13181318@gmail.com>
2024-07-18 18:38:17 +0530

    Add files via upload

---
+++ vraj56     c-- lang.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float p,r,n,vraj;
+	printf("enter the principal amount=");
+	scanf("%f",p);
+	printf("enter the rate=");
+	scanf("%f",r);
+	printf("enter the month");
+	scanf("%f",n);
+	vraj=p*r*n/100;
+	printf("number of this rate=%f",vraj);
+	
+	
+	
+	
+}
